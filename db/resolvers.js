cc0c1c7d11becdc1a7b362173ad2cca177d43438
andres10sa <camand10sa@gmail.com>
2020-12-30 10:29:48 -0500

    json web token

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,32 +1,75 @@
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config({path:'.env'});
 
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        tecnologia: 'Node.js'
-    }, 
-    {
-        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
+const crearToken = (usuario,secreta,expiresIn)=>{
+     console.log(usuario);
+     const {id,email,nombre,apellido}=usuario;
+     //jwt tiene {payload},palabra y {tiempo de expiración}
+     return jwt.sign({id,email,nombre,apellido},secreta,{expiresIn})
+}
+
+
+
 
 //Resolvers
 const resolvers ={
     Query:{
-        //argumentos el input
-        obtenerCursos:(_,{input},ctx)=>{
-          const resultado = cursos.filter(curso=>curso.tecnologia===input.tecnologia);
-          return resultado;
+        obtenerUsuario:async(_,{token})=>{
+            //Leer el jwt
+            const usuarioId = await  jwt.verify(token,process.env.SECRETA)
+            return usuarioId
+        }
+    },
+    Mutation:{
+        nuevoUsuario:async(_,{input})=>{
+
+            const {email,password}=input;
+         
+            //Revisar si el usuario ya está registrado
+            const existeUsuario = await Usuario.findOne({email});
+
+            if(existeUsuario){
+                throw new Error('El usuario ya está registrado')
+            }
+           
+            //Hashear la password
+            const salt = await bcryptjs.genSalt(10);
+            input.password = await bcryptjs.hash(password,salt);
+
+            
+
+            try {
+                //Guardarlo en la base de datos
+                const usuario = new Usuario(input);
+                usuario.save();//guardarlo
+                return usuario;
+            } catch (error) {
+                console.log(error)
+            }
         },
-      
+        autenticarUsuario:async(_,{input})=>{
+             const {email,password}=input;
+            //Si el usuario existe
+             const existeUsuario =await Usuario.findOne({email});
+
+             if(!existeUsuario){
+                throw new Error('El usuario no existe')
+            }
+
+            //Revisar si el password es correcto
+            const passwordCorrecto = await bcryptjs.compare(password,existeUsuario.password);
+            if(!passwordCorrecto){
+                throw new Error('El password es incorrecto')
+            }
+
+            //Crear el token
+            return{
+                token:crearToken(existeUsuario,process.env.SECRETA,'24h')
+            }
+
+        }
     }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -1,24 +1,44 @@
 const {gql} = require('apollo-server');
 
-
+//no permito que type Usuario retorne el password, type Usuario es el modelo que defino y que retorna la función
+//el input UsuarioInput es la data del usuario que voy a crear, es decir el argumento que paso a la función
 //Schema
 const typeDefs=gql`
 
-    type Curso{
-       titulo:String
-    }
-    type Tecnologia{
-        tecnologia:String
-    }
+type Usuario{
+    id:ID
+    nombre:String
+    apellido:String
+    email:String
+    creado:String
+}
 
-    input CursoInput{
-        tecnologia:String
-    }
+type Token{
+    token:String
+}
 
-    type Query{
-        obtenerCursos(input:CursoInput!):[Curso]
-        obtenerTecnologia:[Tecnologia]
-    }
+input UsuarioInput{
+    nombre:String!
+    apellido:String!
+    email:String! 
+    password:String!
+}
+
+input AutenticarInput{
+  email:String!
+  password:String!
+}
+  
+ type Query{
+     obtenerUsuario(token:String!):Usuario
+ }
+
+ type Mutation{
+   nuevoUsuario(input:UsuarioInput):Usuario
+   autenticarUsuario(input:AutenticarInput):Token
+  }
 `;
+
+
 module.exports = typeDefs;
 
